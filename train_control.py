226d99c53297912d5960386e8b84e8c6fb90a90e
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-10-26 12:02:55 -0500

    final commit

--- train_control.py
+++ train_control.py
@@ -4,6 +4,17 @@ import serial
 
 runFlag = False # flag for execution
 
+# Commands
+ringbell = "111111100000101110011101"
+horn = "111111100000101110011100"
+start = "111111100000101111100011"
+trainBrake = "111111100000101110000111"
+boost = "111111100000101110000100"
+trainAcc = "111111100000101111001000"
+trainDec = "111111100000101111000010"
+toggleDir = "111111100000101110000001"
+stop = "111111100000101111100000"
+
 # Open port for serial communication
 try:
     ser = serial.Serial(
@@ -25,16 +36,18 @@ if runFlag:
     # Create the GUI
     sg.theme('SystemDefault')
     layout = [  [
-                    [sg.Button('Ring Bell')],
-                    [sg.Button('Horn')],
-                    [sg.Button('Start')],
-                    [sg.Button('Brake')],
-                    [sg.Button('Accelerate')],
-                    [sg.Button('Decelerate')]
+                    [sg.Button('Ring Bell'), sg.Text('Toggles bell')],
+                    [sg.Button('Horn'), sg.Text('Sounds horn')],
+                    [sg.Button('Start'), sg.Text('Starts engine at low speed')],
+                    [sg.Button('Brake'), sg.Text('Taps the brakes')],
+                    [sg.Button('Accelerate'), sg.Text('Accelerates the train')],
+                    [sg.Button('Decelerate'), sg.Text('Decelerates the train')],
+                    [sg.Button('Toggle Direction'), sg.Text('Toggeles train direction')],
+                    [sg.Button('Stop'), sg.Text('Stops the train')]
                 ]
             ]
 
-    window = sg.Window('Train Control', layout)
+    window = sg.Window('Train Control', layout, resizable=True, size=(300,300))
 
     def sendCommand(command):
         print("Bytes written: ",ser.write(int(command, 2).to_bytes(len(command) // 8, byteorder='big')), " ", command)
@@ -44,25 +57,33 @@ if runFlag:
         event, values = window.read()
         if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
             break
+        # Writing commands for each button press
         elif event == 'Ring Bell':
             print('Ringing Bell...')
-            sendCommand("111111100000101110011101")
+            sendCommand(ringbell)
         elif event == 'Start':
             print('Start')
-            sendCommand("111111100000101111100011")
+            sendCommand(start)
         elif event == 'Accelerate':
             print('Accelerate')
-            sendCommand("111111100000101110000100") # boost
-            sendCommand("111111100000101111001000")
+            sendCommand(boost) # boost
+            sendCommand(trainAcc)
         elif event == 'Decelerate':
             print('Decelerate')
-            sendCommand("111111100000101111000010")
+            sendCommand(trainDec)
         elif event == 'Brake':
             print('Brake')
-            sendCommand("111111100000101110000111")
+            sendCommand(trainBrake)
         elif event == 'Horn':
             print('Horn')
-            sendCommand("111111100000101110011100")
+            sendCommand(horn)
+        elif event == 'Toggle Direction':
+            print('Toggle Direction')
+            sendCommand(toggleDir)
+            sendCommand(start)
+        elif event == 'Stop':
+            print('Stopping')
+            sendCommand(stop)
 
 
     ser.close()
